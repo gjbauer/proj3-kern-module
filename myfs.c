16c7d246a38ea692510e2ef61dc229c1591a07b2
gjbauer <gabeb1277@gmail.com>
2025-09-30 23:20:57 -0400

    Create myfs.c

---
+++ myfs.c
@@ -0,0 +1,442 @@
+/*
+ * Copyright (c) 2024 Your Name
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/kernel.h>
+#include <sys/module.h>
+#include <sys/mount.h>
+#include <sys/stat.h>
+#include <sys/vnode.h>
+#include <sys/bio.h>
+#include <sys/buf.h>
+#include <sys/dirent.h>
+#include <sys/malloc.h>
+#include <sys/queue.h>
+#include <sys/lock.h>
+#include <sys/condvar.h>
+
+#define MYFS_MAGIC 0x4D594653  // "MYFS" in hex
+#define MYFS_NAME "myfs"
+#define MYFS_VERSION 1
+
+/* Filesystem superblock information */
+struct myfs_sb {
+    uint32_t magic;
+    uint64_t total_blocks;
+    uint64_t free_blocks;
+    // Add your superblock data here
+};
+
+/* Filesystem mount structure */
+struct myfs_mount {
+    struct mount *mp;
+    struct myfs_sb sb;
+    // Add mount-specific data here
+};
+
+/* Vnode data */
+struct myfs_node {
+    ino_t ino;
+    mode_t mode;
+    nlink_t nlink;
+    off_t size;
+    struct timespec atime;
+    struct timespec mtime;
+    struct timespec ctime;
+    // Add node-specific data here
+};
+
+/* Function declarations */
+static int myfs_mount(struct mount *mp);
+static int myfs_unmount(struct mount *mp);
+static int myfs_root(struct mount *mp, struct vnode **vpp);
+static int myfs_statfs(struct mount *mp, struct statfs *sbp);
+static int myfs_vget(struct mount *mp, ino_t ino, struct vnode **vpp);
+
+/* VFS operations vector */
+static struct vfsops myfs_vfsops = {
+    .vfs_mount = myfs_mount,
+    .vfs_unmount = myfs_unmount,
+    .vfs_root = myfs_root,
+    .vfs_statfs = myfs_statfs,
+    .vfs_vget = myfs_vget,
+    .vfs_sync = vfs_stdsync,
+    .vfs_init = NULL,
+    .vfs_uninit = NULL,
+};
+
+/* Vnode operations */
+static int myfs_lookup(struct vop_lookup_args *ap);
+static int myfs_create(struct vop_create_args *ap);
+static int myfs_mknod(struct vop_mknod_args *ap);
+static int myfs_open(struct vop_open_args *ap);
+static int myfs_close(struct vop_close_args *ap);
+static int myfs_access(struct vop_access_args *ap);
+static int myfs_getattr(struct vop_getattr_args *ap);
+static int myfs_setattr(struct vop_setattr_args *ap);
+static int myfs_read(struct vop_read_args *ap);
+static int myfs_write(struct vop_write_args *ap);
+static int myfs_ioctl(struct vop_ioctl_args *ap);
+static int myfs_poll(struct vop_poll_args *ap);
+static int myfs_reclaim(struct vop_reclaim_args *ap);
+static int myfs_readdir(struct vop_readdir_args *ap);
+static int myfs_readlink(struct vop_readlink_args *ap);
+static int myfs_symlink(struct vop_symlink_args *ap);
+static int myfs_remove(struct vop_remove_args *ap);
+static int myfs_rename(struct vop_rename_args *ap);
+static int myfs_mkdir(struct vop_mkdir_args *ap);
+static int myfs_rmdir(struct vop_rmdir_args *ap);
+static int myfs_inactive(struct vop_inactive_args *ap);
+static int myfs_truncate(struct vop_truncate_args *ap);
+
+/* Vnode operations vector */
+static struct vop_ops myfs_vops = {
+    .vop_default = &default_vnodeops,
+
+    /* Directory operations */
+    .vop_lookup = myfs_lookup,
+    .vop_create = myfs_create,
+    .vop_mknod = myfs_mknod,
+    .vop_open = myfs_open,
+    .vop_close = myfs_close,
+    .vop_access = myfs_access,
+    .vop_getattr = myfs_getattr,
+    .vop_setattr = myfs_setattr,
+    .vop_read = myfs_read,
+    .vop_write = myfs_write,
+    .vop_ioctl = myfs_ioctl,
+    .vop_poll = myfs_poll,
+    .vop_reclaim = myfs_reclaim,
+    .vop_readdir = myfs_readdir,
+    .vop_readlink = myfs_readlink,
+    .vop_symlink = myfs_symlink,
+    .vop_remove = myfs_remove,
+    .vop_rename = myfs_rename,
+    .vop_mkdir = myfs_mkdir,
+    .vop_rmdir = myfs_rmdir,
+    .vop_inactive = myfs_inactive,
+    .vop_truncate = myfs_truncate,
+};
+
+/* Mount function */
+static int
+myfs_mount(struct mount *mp)
+{
+    struct myfs_mount *mmp;
+    int error = 0;
+
+    printf("MYFS: Mounting filesystem\n");
+
+    /* Allocate mount structure */
+    mmp = malloc(sizeof(struct myfs_mount), M_TEMP, M_WAITOK | M_ZERO);
+    if (!mmp)
+        return (ENOMEM);
+
+    mp->mnt_data = mmp;
+    mmp->mp = mp;
+
+    /* Initialize superblock */
+    mmp->sb.magic = MYFS_MAGIC;
+    mmp->sb.total_blocks = 0;  // Initialize with actual values
+    mmp->sb.free_blocks = 0;
+
+    /* Set filesystem statistics */
+    mp->mnt_stat.f_iosize = PAGE_SIZE;
+    mp->mnt_stat.f_blocks = mmp->sb.total_blocks;
+    mp->mnt_stat.f_bfree = mmp->sb.free_blocks;
+    mp->mnt_stat.f_bavail = mmp->sb.free_blocks;
+    mp->mnt_stat.f_namemax = NAME_MAX;
+
+    /* Set VFS flags */
+    vfs_getnewfsid(mp);
+    mp->mnt_flag |= MNT_LOCAL;
+
+    return (error);
+}
+
+/* Unmount function */
+static int
+myfs_unmount(struct mount *mp)
+{
+    struct myfs_mount *mmp = (struct myfs_mount *)mp->mnt_data;
+
+    printf("MYFS: Unmounting filesystem\n");
+
+    if (mmp) {
+        free(mmp, M_TEMP);
+        mp->mnt_data = NULL;
+    }
+
+    return (0);
+}
+
+/* Root vnode function */
+static int
+myfs_root(struct mount *mp, struct vnode **vpp)
+{
+    // Implement getting root directory vnode
+    printf("MYFS: Getting root vnode\n");
+    return (ENOSYS);
+}
+
+/* Statfs function */
+static int
+myfs_statfs(struct mount *mp, struct statfs *sbp)
+{
+    struct myfs_mount *mmp = (struct myfs_mount *)mp->mnt_data;
+
+    printf("MYFS: Getting filesystem statistics\n");
+
+    sbp->f_blocks = mmp->sb.total_blocks;
+    sbp->f_bfree = mmp->sb.free_blocks;
+    sbp->f_bavail = mmp->sb.free_blocks;
+    sbp->f_files = 0;  // Total inodes
+    sbp->f_ffree = 0;  // Free inodes
+    sbp->f_bsize = PAGE_SIZE;
+    sbp->f_iosize = PAGE_SIZE;
+    sbp->f_namemax = NAME_MAX;
+
+    return (0);
+}
+
+/* Vget function - get vnode by inode number */
+static int
+myfs_vget(struct mount *mp, ino_t ino, struct vnode **vpp)
+{
+    // Implement getting vnode by inode number
+    printf("MYFS: Getting vnode for ino %ju\n", (uintmax_t)ino);
+    return (ENOSYS);
+}
+
+/* Vnode operations implementation */
+
+static int
+myfs_lookup(struct vop_lookup_args *ap)
+{
+    printf("MYFS: Lookup operation\n");
+    return (ENOSYS);
+}
+
+static int
+myfs_create(struct vop_create_args *ap)
+{
+    printf("MYFS: Create operation\n");
+    return (ENOSYS);
+}
+
+static int
+myfs_mknod(struct vop_mknod_args *ap)
+{
+    printf("MYFS: Mknod operation\n");
+    return (ENOSYS);
+}
+
+static int
+myfs_open(struct vop_open_args *ap)
+{
+    printf("MYFS: Open operation\n");
+    return (0);
+}
+
+static int
+myfs_close(struct vop_close_args *ap)
+{
+    printf("MYFS: Close operation\n");
+    return (0);
+}
+
+static int
+myfs_access(struct vop_access_args *ap)
+{
+    printf("MYFS: Access operation\n");
+    return (ENOSYS);
+}
+
+static int
+myfs_getattr(struct vop_getattr_args *ap)
+{
+    printf("MYFS: Getattr operation\n");
+    return (ENOSYS);
+}
+
+static int
+myfs_setattr(struct vop_setattr_args *ap)
+{
+    printf("MYFS: Setattr operation\n");
+    return (ENOSYS);
+}
+
+static int
+myfs_read(struct vop_read_args *ap)
+{
+    printf("MYFS: Read operation\n");
+    return (ENOSYS);
+}
+
+static int
+myfs_write(struct vop_write_args *ap)
+{
+    printf("MYFS: Write operation\n");
+    return (ENOSYS);
+}
+
+static int
+myfs_ioctl(struct vop_ioctl_args *ap)
+{
+    printf("MYFS: Ioctl operation\n");
+    return (ENOTTY);
+}
+
+static int
+myfs_poll(struct vop_poll_args *ap)
+{
+    printf("MYFS: Poll operation\n");
+    return (ENOSYS);
+}
+
+static int
+myfs_reclaim(struct vop_reclaim_args *ap)
+{
+    struct vnode *vp = ap->a_vp;
+    struct myfs_node *node;
+
+    printf("MYFS: Reclaim vnode\n");
+
+    node = (struct myfs_node *)vp->v_data;
+    if (node) {
+        free(node, M_TEMP);
+        vp->v_data = NULL;
+    }
+
+    return (0);
+}
+
+static int
+myfs_readdir(struct vop_readdir_args *ap)
+{
+    printf("MYFS: Readdir operation\n");
+    return (ENOSYS);
+}
+
+static int
+myfs_readlink(struct vop_readlink_args *ap)
+{
+    printf("MYFS: Readlink operation\n");
+    return (ENOSYS);
+}
+
+static int
+myfs_symlink(struct vop_symlink_args *ap)
+{
+    printf("MYFS: Symlink operation\n");
+    return (ENOSYS);
+}
+
+static int
+myfs_remove(struct vop_remove_args *ap)
+{
+    printf("MYFS: Remove operation\n");
+    return (ENOSYS);
+}
+
+static int
+myfs_rename(struct vop_rename_args *ap)
+{
+    printf("MYFS: Rename operation\n");
+    return (ENOSYS);
+}
+
+static int
+myfs_mkdir(struct vop_mkdir_args *ap)
+{
+    printf("MYFS: Mkdir operation\n");
+    return (ENOSYS);
+}
+
+static int
+myfs_rmdir(struct vop_rmdir_args *ap)
+{
+    printf("MYFS: Rmdir operation\n");
+    return (ENOSYS);
+}
+
+static int
+myfs_inactive(struct vop_inactive_args *ap)
+{
+    printf("MYFS: Inactive operation\n");
+    return (0);
+}
+
+static int
+myfs_truncate(struct vop_truncate_args *ap)
+{
+    printf("MYFS: Truncate operation\n");
+    return (ENOSYS);
+}
+
+/* Module event handler */
+static int
+myfs_modevent(module_t mod, int type, void *data)
+{
+    int error = 0;
+
+    switch (type) {
+    case MOD_LOAD:
+        printf("MYFS: Loading filesystem module\n");
+        error = vfs_attach(&myfs_vfsops);
+        if (error) {
+            printf("MYFS: Failed to attach VFS ops: %d\n", error);
+            break;
+        }
+        break;
+
+    case MOD_UNLOAD:
+        printf("MYFS: Unloading filesystem module\n");
+        error = vfs_detach(&myfs_vfsops);
+        if (error) {
+            printf("MYFS: Failed to detach VFS ops: %d\n", error);
+        }
+        break;
+
+    case MOD_SHUTDOWN:
+        printf("MYFS: Shutdown requested\n");
+        break;
+
+    default:
+        error = EOPNOTSUPP;
+        break;
+    }
+
+    return (error);
+}
+
+/* Module declaration */
+static moduledata_t myfs_mod = {
+    MYFS_NAME,
+    myfs_modevent,
+    NULL
+};
+
+DECLARE_MODULE(myfs, myfs_mod, SI_SUB_VFS, SI_ORDER_ANY);
+MODULE_VERSION(myfs, MYFS_VERSION);
+MODULE_DEPEND(myfs, opensolaris, 1, 1, 1);
